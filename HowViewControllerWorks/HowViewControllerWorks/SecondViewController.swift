8aefad51d555ec837acf6c04c9ed4b79264dcb69
RinKusChi <meyuzak@gmail.com>
2022-02-03 16:24:42 +0300

    3 February 2022

---
+++ HowViewControllerWorks/HowViewControllerWorks/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  HowViewControllerWorks
+//
+//  Created by Macbook Air on 3.02.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ HowViewControllerWorks/HowViewControllerWorks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HowViewControllerWorks
+//
+//  Created by Macbook Air on 3.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
