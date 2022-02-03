f13b640941e8e1c133196e0bdd1e443341f673c0
RinKusChi <meyuzak@gmail.com>
2022-02-03 17:47:08 +0300

    3 February 2022

---
+++ PresentModally/PresentModally/ViewController2.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController2.swift
+//  PresentModally
+//
+//  Created by Macbook Air on 3.02.2022.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func ClickOnDismiss(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
